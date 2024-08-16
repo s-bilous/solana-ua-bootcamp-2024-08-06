1ee9a2310cfe26f1395dc5774a43169271dba59a
Serhii Bilous <sergey3594@gmail.com>
2024-08-16 22:16:35 +0200

    generate keypair

---
+++ practice-1/generate-keypair.ts
@@ -0,0 +1,6 @@
+import { Keypair } from '@solana/web3.js';
+
+const keypair = Keypair.generate();
+
+console.log(`Public key: ${keypair.publicKey.toBase58()}`);
+console.log(`Secret key: [${keypair.secretKey.join(', ')}]`);
